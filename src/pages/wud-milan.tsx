edf35fe77f34d9228b7d24f8fb917aa608da42d7
Jungho Choi <9rganizedchaos@gmail.com>
2022-07-07 23:37:05 +0900

    :lipstick: #1 스타일 세팅

---
+++ src/pages/wud-milan.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as styles from '../styles/WudMilan.module.scss';
+
+const WudMilanPage = () => {
+    return <div className={styles.wrapper}>This is WudMilan</div>
+}
+
+export default WudMilanPage;
