e039b0aadbd6733d18ecfabe4f3eec96b5e24c5e
Jungho Choi <9rganizedchaos@gmail.com>
2022-07-15 11:54:32 +0900

    :zap: #3 스크롤 성능개선(scroll-overbehave 방지)

--- src/pages/wud-milan.tsx
+++ src/pages/wud-milan.tsx
@@ -1,57 +1,56 @@
 import * as React from 'react';
 import { Menu } from 'react-feather';
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { debounce } from 'lodash';
 import * as styles from '../styles/WudMilan.module.scss';
 
-const ACC = 0.1;
+const ACC = 0.5;
+const DEC = 0.08;
+let bgPivot = 0;
+let mcPivot = 0;
+let rafId: number;
 
 function WudMilanPage() {
-	let bgPivot = 0;
-	let mcPivot = 0;
-	const [rafId, setRafId] = useState<number>(0);
 	const background = useRef<HTMLDivElement>(null);
-	const mainContent = useRef<HTMLDivElement>(null);
+	const main = useRef<HTMLDivElement>(null);
 	const backgroundContent = useRef<HTMLParagraphElement>(null);
-	const mainContentContent = useRef<HTMLElement>(null);
+	const mainContent = useRef<HTMLElement>(null);
 
 	const loop = () => {
-		if (background.current && mainContent.current) {
-			console.log(bgPivot, background.current.scrollLeft);
+		if (background.current && main.current) {
 			background.current.scrollLeft += bgPivot * ACC;
-			mainContent.current.scrollLeft += mcPivot * ACC;
-			bgPivot *= 1 - ACC;
-			mcPivot *= 1 - ACC;
+			main.current.scrollLeft += mcPivot * ACC;
+			bgPivot *= 1 - DEC;
+			mcPivot *= 1 - DEC;
 
 			if (Math.abs(bgPivot) < 1 && Math.abs(mcPivot) < 1) {
 				cancelAnimationFrame(rafId);
 				mcPivot = 0;
 				bgPivot = 0;
 			} else {
-				const newRafId = requestAnimationFrame(loop);
-				setRafId(newRafId);
+				rafId = requestAnimationFrame(loop);
 			}
 		}
 	};
 
-	const handleWindowScroll = async (e: WheelEvent) => {
+	const handleWindowScroll = debounce(async (e: WheelEvent) => {
 		const backgroundContentWidth = backgroundContent.current?.offsetWidth || 1;
-		const mainContentContentWidth = mainContentContent.current?.offsetWidth || 1;
+		const mainContentWidth = mainContent.current?.offsetWidth || 1;
 		const { deltaY } = e;
-		const decelerateDeltaY = deltaY * (backgroundContentWidth / mainContentContentWidth);
+		const decelerateDeltaY = deltaY * (backgroundContentWidth / mainContentWidth);
 
 		if (bgPivot !== 0 && mcPivot !== 0) {
 			await cancelAnimationFrame(rafId);
 		}
 
-		bgPivot = decelerateDeltaY;
-		mcPivot = deltaY;
-		const newRafId = requestAnimationFrame(loop);
-		setRafId(newRafId);
-	};
+		bgPivot = decelerateDeltaY / 2;
+		mcPivot = deltaY / 2;
+		rafId = requestAnimationFrame(loop);
+	}, 100);
 
 	useEffect(() => {
 		window.addEventListener('wheel', (e) => handleWindowScroll(e));
-		return window.removeEventListener('wheel', handleWindowScroll);
+		return window.removeEventListener('wheel', (e) => handleWindowScroll(e));
 	}, []);
 
 	return (
@@ -69,20 +68,20 @@ function WudMilanPage() {
 					</div>
 					<p className={styles.wud_date}>NOVEMBER 11, 2021</p>
 				</header>
-				<div className={styles.scroll_frame} ref={mainContent}>
-					<main ref={mainContentContent}>
-						<section className={styles.hero}>안녕</section>
+				<div className={styles.scroll_frame} ref={main}>
+					<main ref={mainContent}>
+						<section className={styles.hero}></section>
 						<section className={styles.about}>
-							<div className={styles.about_inner}>안녕</div>
+							<div className={styles.about_inner}></div>
 						</section>
 						<section className={styles.talks}>
-							<div className={styles.talks_inner}>안녕</div>
+							<div className={styles.talks_inner}></div>
 						</section>
 						<section className={styles.organizers}>
-							<div className={styles.organizers_inner}>안녕</div>
+							<div className={styles.organizers_inner}></div>
 						</section>
 						<section className={styles.sponsors}>
-							<div className={styles.sponsors_inner}>안녕</div>
+							<div className={styles.sponsors_inner}></div>
 						</section>
 					</main>
 				</div>
