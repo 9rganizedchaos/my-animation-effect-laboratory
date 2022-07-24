370a9b5e880316865d11fb8bf68c3aa117cfad66
Jungho Choi <9rganizedchaos@gmail.com>
2022-07-24 21:05:22 +0900

    :sparkles: #4 PIXIjs 그래픽 추가

--- src/pages/wud-milan.tsx
+++ src/pages/wud-milan.tsx
@@ -3,10 +3,12 @@ import { Menu } from 'react-feather';
 import { useEffect, useRef } from 'react';
 import { debounce } from 'lodash';
 import * as PIXI from 'pixi.js';
+import { Graphics } from 'pixi.js';
 import * as styles from '../styles/WudMilan.module.scss';
 import AboutSection from '../components/wud-milan/AboutSection';
 import TalksSection from '../components/wud-milan/TalksSection';
 import OrganizersSection from '../components/wud-milan/OrganizersSection';
+import { getRandomArbitrary } from '../utils/util';
 
 const ACC = 0.5;
 const DEC = 0.08;
@@ -20,6 +22,9 @@ function WudMilanPage() {
 	const backgroundContent = useRef<HTMLParagraphElement>(null);
 	const mainContent = useRef<HTMLElement>(null);
 	const canvasRef = useRef<HTMLDivElement>(null);
+
+	let mouseX = 0;
+	let mouseY = 0;
 
 	const loop = () => {
 		if (background.current && main.current) {
@@ -55,6 +60,11 @@ function WudMilanPage() {
 		}
 	}, 100);
 
+	const handleMouseMove = debounce((e) => {
+		mouseX = Math.floor(e.pageX / 30);
+		mouseY = Math.floor(e.pageY / 30);
+	}, 10);
+
 	useEffect(() => {
 		window.addEventListener('wheel', (e) => handleWindowScroll(e));
 		return window.removeEventListener('wheel', (e) => handleWindowScroll(e));
@@ -62,25 +72,119 @@ function WudMilanPage() {
 
 	useEffect(() => {
 		if (canvasRef.current) {
+			const canvasWidth = canvasRef.current.offsetWidth;
+			const canvasHeight = canvasRef.current.offsetHeight;
+
 			const app = new PIXI.Application({
-				width: canvasRef.current.offsetWidth,
-				height: canvasRef.current.offsetHeight,
+				width: canvasWidth,
+				height: canvasHeight,
 				backgroundColor: 0x0132de,
 			});
 			canvasRef.current?.appendChild(app.view);
 
-			const graphics = new PIXI.Graphics();
+			const container: Graphics[] = [];
 
-			graphics.beginFill(0xddff6d);
+			setInterval(() => {
+				container.forEach((graphic) => {
+					app.stage.removeChild(graphic);
+				});
 
-			graphics.drawRect(200, 200, 50, 50);
+				for (let i = 0; i < 200; i++) {
+					const whitePixel = new PIXI.Graphics();
+					const greenPixel1 = new PIXI.Graphics();
+					const greenPixel2 = new PIXI.Graphics();
 
-			app.stage.addChild(graphics);
+					whitePixel.beginFill(0xffffff);
+					greenPixel1.beginFill(0xddff6d);
+					greenPixel2.beginFill(0xddff6d);
+
+					const whitePixelTheta = Math.random() * 6 * Math.PI;
+					const greenPixelTheta1 = Math.random() * 8 * Math.PI;
+					const greenPixelTheta2 = Math.random() * 8 * Math.PI;
+					const whitePixelR = Math.random() * 6;
+					const greenPixelR1 = Math.random() * 8;
+					const greenPixelR2 = Math.random() * 8;
+
+					whitePixel.drawRect(
+						mouseX * 30 + Math.floor(whitePixelR * Math.cos(whitePixelTheta)) * 30,
+						mouseY * 30 + Math.floor(whitePixelR * Math.sin(whitePixelTheta)) * 30,
+						30,
+						30
+					);
+
+					whitePixel.drawRect(
+						8 * 30 + Math.floor(whitePixelR * Math.cos(whitePixelTheta)) * 30,
+						30 + Math.floor(whitePixelR * Math.sin(whitePixelTheta)) * 30,
+						30,
+						30
+					);
+
+					whitePixel.drawRect(
+						(Math.floor(canvasWidth / 30) - 8) * 30 + Math.floor(whitePixelR * Math.cos(whitePixelTheta)) * 30,
+						(Math.floor(canvasHeight / 30) - 2) * 30 + Math.floor(whitePixelR * Math.sin(whitePixelTheta)) * 30,
+						30,
+						30
+					);
+
+					greenPixel1.drawRect(
+						mouseX * 30 + Math.floor(greenPixelR1 * Math.cos(greenPixelTheta1)) * 30,
+						mouseY * 30 + Math.floor(greenPixelR1 * Math.sin(greenPixelTheta1)) * 30,
+						30,
+						30
+					);
+
+					greenPixel1.drawRect(
+						8 * 30 + Math.floor(greenPixelR1 * Math.cos(greenPixelTheta1)) * 30,
+						30 + Math.floor(greenPixelR1 * Math.sin(greenPixelTheta1)) * 30,
+						30,
+						30
+					);
+
+					greenPixel1.drawRect(
+						(Math.floor(canvasWidth / 30) - 8) * 30 + Math.floor(greenPixelR1 * Math.cos(greenPixelTheta1)) * 30,
+						(Math.floor(canvasHeight / 30) - 2) * 30 + Math.floor(greenPixelR1 * Math.sin(greenPixelTheta1)) * 30,
+						30,
+						30
+					);
+
+					greenPixel2.drawRect(
+						mouseX * 30 + Math.floor(greenPixelR2 * Math.cos(greenPixelTheta2)) * 30,
+						mouseY * 30 + Math.floor(greenPixelR2 * Math.sin(greenPixelTheta2)) * 30,
+						30,
+						30
+					);
+
+					greenPixel2.drawRect(
+						8 * 30 + Math.floor(greenPixelR2 * Math.cos(greenPixelTheta2)) * 30,
+						30 + Math.floor(greenPixelR2 * Math.sin(greenPixelTheta2)) * 30,
+						30,
+						30
+					);
+
+					greenPixel2.drawRect(
+						(Math.floor(canvasWidth / 30) - 8) * 30 + Math.floor(greenPixelR2 * Math.cos(greenPixelTheta2)) * 30,
+						(Math.floor(canvasHeight / 30) - 2) * 30 + Math.floor(greenPixelR2 * Math.sin(greenPixelTheta2)) * 30,
+						30,
+						30
+					);
+
+					app.stage.addChild(whitePixel);
+					app.stage.addChild(greenPixel1);
+					app.stage.addChild(greenPixel2);
+
+					container.push(whitePixel, greenPixel1, greenPixel2);
+				}
+			}, 500);
 		}
 
 		return () => {
 			canvasRef.current?.removeChild(app.view);
 		};
+	});
+
+	useEffect(() => {
+		window.addEventListener('mousemove', (e) => handleMouseMove(e));
+		return window.removeEventListener('mousemove', (e) => handleMouseMove(e));
 	});
 
 	return (
